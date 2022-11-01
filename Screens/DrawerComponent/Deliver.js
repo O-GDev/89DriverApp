17ad8b026f4a63f1389ee59ba1a1400bab585207
anie308 <aniebietabasiobo@gmail.com>
2022-11-01 01:33:56 +0100

    Revert "api connect"
    This reverts commit f654ea31d1bdcc0f8b4803c369b938c04689414d.

--- Screens/DrawerComponent/Deliver.js
+++ Screens/DrawerComponent/Deliver.js
@@ -2,76 +2,13 @@ import { View, Text, TouchableOpacity,StatusBar,SafeAreaView,FlatList } from 're
 import React from 'react'
 import { FontAwesome5 } from '@expo/vector-icons'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { useEffect } from 'react';
-import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState } from 'react';
-import Toast from 'react-native-root-toast';
-
+import { useNavigation } from '@react-navigation/native';
 
 export default function Deliver() {
-const route = useRoute()
   const navigation = useNavigation();
-  const requestId = route.params?.requestId
-  const [request, setRequest] = useState(null)
-  // console.warn(requestId)
-  
- 
-  useEffect(()=> {
-  const findRequest = async () => {
-    const token = await AsyncStorage.getItem("user");
-      const authToken = JSON.parse(token);
-      let config = {
-        headers: {
-          Authorization: `Token ${authToken}`,
-        },
-      };
-     
-    const driverRequest = await  axios.get(
-      `${apiUrl}/api/driverrequest/`,
-      config
-    );
-    if (driverRequest?.data.status === true) {
-      const requests = driverRequest?.data.job;
-      const singleRequest = requests?.find(request => request.id === requestId)
-      if(singleRequest){
-        setRequest(singleRequest)
-      }
-
-    }
-    
-  }
-    findRequest()
-  })
-
-  
-  // console.warn(request)
-
-
-  const acceptRequest = async () => {
-    const token = await AsyncStorage.getItem("user");
-    const authToken = JSON.parse(token);
-    let config = {
-      headers: {
-        Authorization: `Token ${authToken}`,
-      },
-    };
-    const newStatus = {
-      id: request.id,
-      status: 'Accept'
-    }
-    const updateTask = await axios.patch(`${apiUrl}/api/driverrequest/`, newStatus, config);
-    if(updateTask.data.status === true){
-      navigation.navigate('Del', {requestDetails: request})
-      Toast.show('Reuest Accepted')
-    }
-  }
- 
   return (
     
-    request ? (
-      <View>
+    <View>
       <StatusBar/>
       <SafeAreaView style={[GlobalStyle.horizontal_center_screen, { backgroundColor: 'white', height: '100%' }]} />
      <View style={{height:200,width:'100%'}} >
@@ -86,18 +23,17 @@ const route = useRoute()
     longitudeDelta: 0.0421}}
  />
       </View>
-      <Text style={{paddingLeft:15,paddingTop:15,fontSize:17,fontWeight:'600'}}>{request?.resturant_name || 'loading...'}</Text>
+      <Text style={{paddingLeft:15,paddingTop:15,fontSize:17,fontWeight:'600'}}>Frozen Food Joint</Text>
           <Text style={{fontSize: 15,paddingLeft:15 }}>
           <FontAwesome5 name='map-marker-alt' color={"blue"} size={30} />
-          {request?.delivery_address || 'loading...'}
+          2324 Murphy Court, East Derry, New Hampshire
           </Text>
          <View style={{paddingRight:10,paddingTop:15,flexDirection:'row',justifyContent:'flex-end'}}>
          <TouchableOpacity onPress={() => navigation.navigate('Home')} style={{backgroundColor:'blue',borderRadius:25,width:'40%',justifyContent:'center',alignSelf:'flex-end',alignContent:'center',height:40,}}><Text style={{justifyContent:'center',color:'white',fontWeight:'600',textAlign:'center',fontSize:18}} >reject request</Text></TouchableOpacity>
          <View style={{borderWidth:4,color:'white',opacity:0}}/>
-         <TouchableOpacity onPress={acceptRequest} style={{backgroundColor:'blue',borderRadius:25,width:'40%',justifyContent:'center',alignSelf:'flex-end',alignContent:'center',height:40,}}><Text style={{justifyContent:'center',color:'white',fontWeight:'600',textAlign:'center',fontSize:18}} >accept request</Text></TouchableOpacity>
+         <TouchableOpacity onPress={() => navigation.navigate('Del')} style={{backgroundColor:'blue',borderRadius:25,width:'40%',justifyContent:'center',alignSelf:'flex-end',alignContent:'center',height:40,}}><Text style={{justifyContent:'center',color:'white',fontWeight:'600',textAlign:'center',fontSize:18}} >accept request</Text></TouchableOpacity>
          </View>
          </View>
-    ) : (null)
     
   )
 }
--- components/Check/BackgroundCheck.js
+++ components/Check/BackgroundCheck.js
@@ -39,8 +39,36 @@ const [selectedItem, setSelectedItem] = useState(null)
 
 
 
+<<<<<<< HEAD
 
   
+=======
+    let config = {
+      headers: {
+        Authorization: `Token ${authToken}`,
+      },
+    };
+
+    const driverProfile = await axios.post(
+      `${apiUrl}/api/driver/`,
+      driverInfo,
+       config
+    );
+
+    if(driverProfile.data.status === true){
+      Toast.show('Driver Created Successfully')
+      navigation.navigate("Vehicle Info");
+    }else{
+      Toast.show(driverProfile.data.status)
+    }
+
+    // } else{
+
+
+    // }
+  };
+
+>>>>>>> parent of f654ea3 (api connect)
   return (
     <KeyboardAvoidingView>
       <ScrollView style={{backgroundColor:'white'}}>
